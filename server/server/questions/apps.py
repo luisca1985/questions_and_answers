8d96bbaae1127d9798d9c2edb44c358b39fcba56
Luis Camilo Jimenez Alvarez <luisca195@gmail.com>
2022-01-06 17:21:09 -0500

    Include app questions in server

--- server/server/questions/apps.py
+++ server/server/questions/apps.py
@@ -4,5 +4,5 @@ from django.apps import AppConfig
 class QuestionsAppConfig(AppConfig):
     """Questions app config"""
     # default_auto_field = 'django.db.models.BigAutoField'
-    name = 'questions'
+    name = 'server.questions'
     verbose_name = 'Questions'
