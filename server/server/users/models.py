c1d6ae476e5c4eacf9e2f77a33736f505dac6d5e
Luis Camilo Jimenez Alvarez <luisca195@gmail.com>
2022-01-06 18:15:33 -0500

    Configure model User

--- server/server/users/models.py
+++ server/server/users/models.py
@@ -2,12 +2,36 @@
 
 # Django
 from django.db import models
-from django.contrib.auth.models import AbstractUser 
+from django.contrib.auth.models import AbstractUser
 
 # Utilities
 from server.utils.models import QAModel
 
 # Create your models here.
 
+
 class Users(QAModel, AbstractUser):
-    pass
+    """User model.
+
+    Extend from Django's Abstract User, change the username field
+    to email and add some extra fields.
+    """
+    email = models.EmailField(
+        'email_address',
+        unique=True,
+        error_messages={
+            'unique': 'A user with that email already exists'
+        }
+
+        USERNAME_FIELD='email'
+        REQUIRED_FIELDS=['username', 'first_name', 'last_name']
+
+        def __str__(self):
+        """Return username"""
+        return self.username
+
+        def get_short_name(self):
+        """Return username"""
+        return self.username
+
+    )
