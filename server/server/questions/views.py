bf43b4db25b2bb083e79e9c5390522f39dda903b
Luis Camilo Jimenez Alvarez <luisca195@gmail.com>
2022-01-07 01:35:47 -0500

    create question view

--- server/server/questions/views.py
+++ server/server/questions/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+# Django Rest Framework
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+# Models
+from server.questions.models import Question
+
+# Serializers
+from server.questions.serializers import QuestionSerializer
+
+
+class QuestionViewSet(viewsets.ViewSet):
+    def list(self, request):  # /api/questions
+        questions = Question.objects.all()
+        serializer = QuestionSerializer(questions, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):  # /api/questions
+        pass
+
+    def retrieve(self, request, pk=None): # /api/questions/<str:id>
+        pass
+
+    def update(self, request, pk=None): # /api/questions/<str:id>
+        pass
+
+    def destroy(self, request, pk=None): # /api/questions/<str:id>
+        pass
