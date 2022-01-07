b7d83ee1d00d155ba4dc9e2e31fc3c1e3afe67b5
Luis Camilo Jimenez Alvarez <luisca195@gmail.com>
2022-01-06 23:54:30 -0500

    inlcude question in answer admin

--- server/server/questions/admin.py
+++ server/server/questions/admin.py
@@ -8,23 +8,24 @@ from django.contrib import admin
 from server.questions.models import Question, Answer
 
 
-
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
     """Question model admin."""
 
-    list_display = ('asked_by','title','detail', 'answers_made', 'is_resolved', 'is_closed', 'is_public')
-    search_fields = ('asked_by__username','asked_by__email')
-    list_filter = ('asked_by__username','asked_by__email','is_resolved', 'is_closed', 'is_public', 'created', 'modified')
+    list_display = ('asked_by', 'title', 'detail', 'answers_made',
+                    'is_resolved', 'is_closed', 'is_public')
+    search_fields = ('asked_by__username', 'asked_by__email')
+    list_filter = ('asked_by__username', 'asked_by__email',
+                   'is_resolved', 'is_closed', 'is_public', 'created', 'modified')
+
 
 @admin.register(Answer)
 class AnswerAdmin(admin.ModelAdmin):
     """Answer model admin."""
 
-    list_display = ('answered_by','detail', 'is_correct' )
-    search_fields = ('answered_by__username','answered_by__email'
-    # 'question__title',
-    )
-    list_filter = ('answered_by__username','answered_by__email','is_correct', 'created', 'modified',
-        # 'question__title',
-    )
+    list_display = ('question_to_answer', 'answered_by',
+                    'detail', 'is_correct')
+    search_fields = ('question_to_answer__title',
+                     'answered_by__username', 'answered_by__email')
+    list_filter = ('question_to_answer__title', 'answered_by__username',
+                   'answered_by__email', 'is_correct', 'created', 'modified')
