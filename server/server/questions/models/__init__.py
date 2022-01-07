b9e31c93589291017a805896ae4b700ab24ddabf
Luis Camilo Jimenez Alvarez <luisca195@gmail.com>
2022-01-06 22:06:05 -0500

    Create model answers

--- server/server/questions/models/__init__.py
+++ server/server/questions/models/__init__.py
@@ -1,1 +1,2 @@
 from .questions import Question
+from .answers import Answer
---
+++ server/server/questions/models/answers.py
@@ -0,0 +1,33 @@
+"""Answer model."""
+
+# Django
+from django.db import models
+
+# Utilities
+from server.utils.models import QAModel
+
+# Questions
+from .questions import Question
+
+
+class Answer(QAModel):
+    """Answer model.
+
+    Answer made for a user.
+    """
+    # question = Question
+    detail = models.CharField('Answer detail', max_length=500)
+    is_correct =  models.BooleanField(
+        'this is the correct answer',
+        default=False,
+        help_text='This is the correct answer select by the user who asked the question.'
+    )
+
+    # def __str__(self):
+    # """Returns question's str representation"""
+    # return str(self.title)
+
+    class Meta(QAModel.Meta):
+        """Meta class"""
+
+        ordering = ['-is_correct', '-modified', '-created']
