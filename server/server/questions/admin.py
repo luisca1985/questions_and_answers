7f6280ef06f49548810695f9f3f080abf5466483
Luis Camilo Jimenez Alvarez <luisca195@gmail.com>
2022-01-06 22:41:14 -0500

    question & answer models admin is cofigure

--- server/server/questions/admin.py
+++ server/server/questions/admin.py
@@ -1,3 +1,28 @@
+"""User models admin."""
+
+# Django
 from django.contrib import admin
 
-# Register your models here.
+# Models
+
+from server.questions.models import Question, Answer
+
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    """Question model admin."""
+
+    list_display = ('title','detail', 'answers_made', 'is_resolved', 'is_closed', 'is_public' )
+    list_filter = ('is_resolved', 'is_closed', 'is_public', 'created', 'modified')
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    """Answer model admin."""
+
+    list_display = ('detail', 'is_correct' )
+    # search_fields = ('question__title',)
+    list_filter = (
+        # 'question__title',
+        'is_correct',
+        )
