029e18fe8441696fbb24ff00c2ffb47d05026336
Luis Camilo Jimenez Alvarez <luisca195@gmail.com>
2022-01-07 01:35:00 -0500

    update path import model question

--- server/server/questions/serializers.py
+++ server/server/questions/serializers.py
@@ -1,7 +1,7 @@
 #  Django Rest Framework
 from rest_framework import fields, serializers
 
-from questions.models import Question
+from server.questions.models import Question
 
 class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
