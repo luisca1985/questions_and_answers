bd1d50a1dedf4ff9a405a2f1a5e492598d93c07f
Luis Camilo Jimenez Alvarez <luisca195@gmail.com>
2022-01-06 19:22:13 -0500

    Change class Users to User in users model

--- server/server/users/models.py
+++ server/server/users/models.py
@@ -10,7 +10,7 @@ from server.utils.models import QAModel
 # Create your models here.
 
 
-class Users(QAModel, AbstractUser):
+class User(QAModel, AbstractUser):
     """User model.
 
     Extend from Django's Abstract User, change the username field
@@ -22,16 +22,15 @@ class Users(QAModel, AbstractUser):
         error_messages={
             'unique': 'A user with that email already exists'
         }
-
-        USERNAME_FIELD='email'
-        REQUIRED_FIELDS=['username', 'first_name', 'last_name']
-
-        def __str__(self):
-        """Return username"""
-        return self.username
-
-        def get_short_name(self):
-        """Return username"""
-        return self.username
-
     )
+
+    USERNAME_FIELD='email'
+    REQUIRED_FIELDS=['username', 'first_name', 'last_name']
+
+    def __str__(self):
+        """Return username"""
+        return self.username
+
+    def get_short_name(self):
+        """Return username"""
+        return self.username
