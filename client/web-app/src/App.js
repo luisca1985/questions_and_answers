67d7f3cbaa969bc0eadb99ba72654f2c887f8242
Luis Camilo Jimenez Alvarez <luisca195@gmail.com>
2022-01-08 10:14:12 -0500

    checking proxy conf

--- client/web-app/src/App.js
+++ client/web-app/src/App.js
@@ -6,11 +6,13 @@ function App() {
 
   const [initialState, setState] = useState([])
 
-  const url = 'http://localhost:8000/api/answers/1'
+  // const url = '/api/answers/1'
+  const url = '/api'
 
   useEffect(()=>{
     fetch(url).then(response => {
       if(response.status === 200){
+        console.log(response)
         return response.json()
       }
     }).then(data => setState(data))
