7feb9c50c5052d1b27a8c448740d74fa2b7c664b
Luis Camilo Jimenez Alvarez <luisca1985@gmail.com>
2022-01-10 03:40:18 -0500

    adjust nav bar to create a show questions

--- client/web-app/src/Components/Nav.tsx
+++ client/web-app/src/Components/Nav.tsx
@@ -1,21 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+
 
 const Nav = () => {
+    const options = [{value:"Show questions", url:"/questions"},{value:"Create question", url:"/questions/create"}]
     return (
         <div className="nav-scroller py-1 mb-2">
             <nav className="nav d-flex justify-content-between">
-                <a className="p-2 link-secondary" href="#">World</a>
-                <a className="p-2 link-secondary" href="#">U.S.</a>
-                <a className="p-2 link-secondary" href="#">Technology</a>
-                <a className="p-2 link-secondary" href="#">Design</a>
-                <a className="p-2 link-secondary" href="#">Culture</a>
-                <a className="p-2 link-secondary" href="#">Business</a>
-                <a className="p-2 link-secondary" href="#">Politics</a>
-                <a className="p-2 link-secondary" href="#">Opinion</a>
-                <a className="p-2 link-secondary" href="#">Science</a>
-                <a className="p-2 link-secondary" href="#">Health</a>
-                <a className="p-2 link-secondary" href="#">Style</a>
-                <a className="p-2 link-secondary" href="#">Travel</a>
+                {
+                options.map((option)=>
+                    <Link to={option.url} className="p-2 link-secondary">{option.value}</Link>
+                )
+                }
             </nav>
         </div>
     );
