1069cfca25ebb9ad1cf1b1a58b0d501e3da995ef
Luis Camilo Jimenez Alvarez <luisca1985@gmail.com>
2022-01-10 00:41:40 -0500

    Configurate module question to list products

--- client/web-app/src/Modules/Questions.tsx
+++ client/web-app/src/Modules/Questions.tsx
@@ -1,30 +1,34 @@
 import React from 'react';
 import BriefInfoBlock from '../Components/BriefInfoBlock';
+import {useState, useEffect} from 'react';
+import { Question as QuestionInterface } from '../Interfaces/Questions';
 
 const Questions = () => {
-    return (
-        <div className="row mb-2">
-        <div className="col-md-6">
-          <BriefInfoBlock
-           tag=""
-           title="Featured post" 
-           date="Nov 12"/>
-        </div>
-        <div className="col-md-6">
-          <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-            <div className="col p-4 d-flex flex-column position-static">
-              <strong className="d-inline-block mb-2 text-success">Design</strong>
-              <h3 className="mb-0">Post title</h3>
-              <div className="mb-1 text-muted">Nov 11</div>
-              <p className="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" className="stretched-link">Continue reading</a>
-            </div>
-            <div className="col-auto d-none d-lg-block">
-              <svg className="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+    const [questionList, setQuestions] = useState([])
+    const url = '/api/questions'
+    
+    useEffect(()=>{
+      fetch(url).then(response => {
+        if(response.status === 200){
+          return response.json()
+        }
+      }).then(data => setQuestions(data))
+    })
 
+    return (
+      <div className="row">
+        {
+          questionList.map((question:QuestionInterface) =>
+            <div className="col-md-6">
+              <BriefInfoBlock  
+                key={question.id} 
+                tag="" 
+                title={question.title} 
+                date='Nov 12'
+                detail={ question.detail }/>
             </div>
-          </div>
-        </div>
+          )
+        }
       </div>
     );
 };
