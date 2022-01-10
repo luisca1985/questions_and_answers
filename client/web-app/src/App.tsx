872df25e97e6dd76324a731dea341209dd640cae
Luis Camilo Jimenez Alvarez <luisca1985@gmail.com>
2022-01-09 22:37:20 -0500

    separate Nav, Header, Display, Questions

--- client/web-app/src/App.tsx
+++ client/web-app/src/App.tsx
@@ -1,89 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Nav from './Components/Nav';
+import Header from './Components/Header';
+import Display from './Components/Display';
+import Questions from './Modules/Questions';
 
 function App() {
   return (
     <div className="App">
       {/* view-source:https://getbootstrap.com/docs/5.1/examples/blog/ */}
       <div className="container">
-        <header className="blog-header py-3">
-          <div className="row flex-nowrap justify-content-between align-items-center">
-            <div className="col-4 pt-1">
-              <a className="link-secondary" href="#">Subscribe</a>
-            </div>
-            <div className="col-4 text-center">
-              <a className="blog-header-logo text-dark" href="#">Large</a>
-            </div>
-            <div className="col-4 d-flex justify-content-end align-items-center">
-              <a className="link-secondary" href="#" aria-label="Search">
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="mx-3" role="img" viewBox="0 0 24 24"><title>Search</title><circle cx="10.5" cy="10.5" r="7.5"/><path d="M21 21l-5.2-5.2"/></svg>
-              </a>
-              <a className="btn btn-sm btn-outline-secondary" href="#">Sign up</a>
-            </div>
-          </div>
-        </header>
-
-        <div className="nav-scroller py-1 mb-2">
-          <nav className="nav d-flex justify-content-between">
-            <a className="p-2 link-secondary" href="#">World</a>
-            <a className="p-2 link-secondary" href="#">U.S.</a>
-            <a className="p-2 link-secondary" href="#">Technology</a>
-            <a className="p-2 link-secondary" href="#">Design</a>
-            <a className="p-2 link-secondary" href="#">Culture</a>
-            <a className="p-2 link-secondary" href="#">Business</a>
-            <a className="p-2 link-secondary" href="#">Politics</a>
-            <a className="p-2 link-secondary" href="#">Opinion</a>
-            <a className="p-2 link-secondary" href="#">Science</a>
-            <a className="p-2 link-secondary" href="#">Health</a>
-            <a className="p-2 link-secondary" href="#">Style</a>
-            <a className="p-2 link-secondary" href="#">Travel</a>
-          </nav>
-        </div>
+        <Header/>
+        <Nav/>
       </div>
-
       <main className="container">
-        <div className="p-4 p-md-5 mb-4 text-white rounded bg-dark">
-          <div className="col-md-6 px-0">
-            <h1 className="display-4 fst-italic">Title of a longer featured blog post</h1>
-            <p className="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.</p>
-            <p className="lead mb-0"><a href="#" className="text-white fw-bold">Continue reading...</a></p>
-          </div>
-        </div>
-
-        <div className="row mb-2">
-          <div className="col-md-6">
-            <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-              <div className="col p-4 d-flex flex-column position-static">
-                <strong className="d-inline-block mb-2 text-primary">World</strong>
-                <h3 className="mb-0">Featured post</h3>
-                <div className="mb-1 text-muted">Nov 12</div>
-                <p className="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" className="stretched-link">Continue reading</a>
-              </div>
-              <div className="col-auto d-none d-lg-block">
-                <svg className="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-              </div>
-            </div>
-          </div>
-          <div className="col-md-6">
-            <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-              <div className="col p-4 d-flex flex-column position-static">
-                <strong className="d-inline-block mb-2 text-success">Design</strong>
-                <h3 className="mb-0">Post title</h3>
-                <div className="mb-1 text-muted">Nov 11</div>
-                <p className="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" className="stretched-link">Continue reading</a>
-              </div>
-              <div className="col-auto d-none d-lg-block">
-                <svg className="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-              </div>
-            </div>
-          </div>
-        </div>
-
+        <Display/>
+        <Questions/>
+        
         <div className="row g-5">
           <div className="col-md-8">
             <h3 className="pb-4 mb-4 fst-italic border-bottom">
