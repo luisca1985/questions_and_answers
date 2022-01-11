f12a646c023c3851b452c9774999ed31c033680d
Luis Camilo Jimenez Alvarez <luisca1985@gmail.com>
2022-01-11 14:08:40 -0500

    answers viewset permissions are included

--- server/server/questions/views/answers.py
+++ server/server/questions/views/answers.py
@@ -7,12 +7,14 @@ from server.questions.models import Answer
 
 # Serializers
 from server.questions.serializers import AnswerSerializer
+from server.questions.permissions import AnswerPermission
 
 
 class AnswerViewSet(viewsets.ModelViewSet):
 
     queryset = Answer.objects.all()
     serializer_class = AnswerSerializer
+    permission_classes = (AnswerPermission,)
 
     def list_for_question(self, request, pk=None):  # /api/questions/<str:id>/answers
         answers = Answer.objects.filter(question_to_answer=pk)
