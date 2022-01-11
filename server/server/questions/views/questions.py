fc38533586fde245f1e6b10963a325b86b1b5e3d
Luis Camilo Jimenez Alvarez <luisca1985@gmail.com>
2022-01-11 00:46:02 -0500

    proving filters

--- server/server/questions/views/questions.py
+++ server/server/questions/views/questions.py
@@ -1,15 +1,29 @@
 # Django Rest Framework
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, generics
 from rest_framework.response import Response
 
 # Models
 from server.questions.models import Question
 
+# Filters
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+
 # Serializers
 from server.questions.serializers import QuestionSerializer
 
 
-class QuestionViewSet(viewsets.ViewSet):
+# class QuestionViewSet(viewsets.ViewSet):
+# class QuestionViewSet(viewsets.ModelViewSet):
+class QuestionViewSet(viewsets.ModelViewSet):
+
+
+    # Filters
+    # queryset = Question.objects.all()
+    # serializer_class = QuestionSerializer
+    # filter_backends = (SearchFilter)
+    # search_fields = ['title', 'detail']
+
     def list(self, request):  # /api/questions
         questions = Question.objects.all()
         serializer = QuestionSerializer(questions, many=True)
@@ -37,3 +51,21 @@ class QuestionViewSet(viewsets.ViewSet):
         question = Question.objects.get(id=pk)
         question.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+    # def filter_queryset(self, queryset):
+    #     filter_backends = (DjangoFilterBackend, )
+        
+    #     # Other condition for different filter backend goes here
+    #     for backend in list(filter_backends):
+    #         queryset = backend().filter_queryset(self.request, queryset, view=self)
+    #     return queryset
+    def get_queryset(self):
+        """
+        Optionally restricts the returned purchases to a given user,
+        by filtering against a `username` query parameter in the URL.
+        """
+        queryset = Question.objects.all()
+        title = self.request.query_params.get('title')
+        if title is not None:
+            queryset = queryset.filter(question__title=title)
+        return queryset
