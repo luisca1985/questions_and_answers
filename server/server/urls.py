f68ddede9a1badb3d6a551822852a6a7dedd67a8
Luis Camilo Jimenez Alvarez <luisca1985@gmail.com>
2022-01-11 10:37:50 -0500

    add user path in urls

--- server/server/urls.py
+++ server/server/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('server.questions.urls'))
+    path('api/', include('server.questions.urls')),
+    path('api/', include('server.users.urls'))
 ]
