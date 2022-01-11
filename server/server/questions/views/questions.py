145aa1a271ac136f2c3685c6286ce3e6cadd2d0e
Luis Camilo Jimenez Alvarez <luisca1985@gmail.com>
2022-01-11 14:09:17 -0500

    questions viewset permissions are included

--- server/server/questions/views/questions.py
+++ server/server/questions/views/questions.py
@@ -1,5 +1,7 @@
 # Django Rest Framework
 from rest_framework import  viewsets
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 # Models
 from server.questions.models import Question
@@ -10,6 +12,9 @@ from rest_framework.filters import SearchFilter
 # Serializers
 from server.questions.serializers import QuestionSerializer
 
+# Permission
+from server.questions.permissions import QuestionPermission
+
 
 class QuestionViewSet(viewsets.ModelViewSet):
     queryset = Question.objects.all()
@@ -17,3 +22,5 @@ class QuestionViewSet(viewsets.ModelViewSet):
     # Filters
     filter_backends = [SearchFilter]
     search_fields = ['title', 'detail']
+    # Permissions
+    permission_classes = (QuestionPermission,)
