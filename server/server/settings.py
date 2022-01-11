db023240997bac70291f7fbc5df9e12796eb778e
Luis Camilo Jimenez Alvarez <luisca1985@gmail.com>
2022-01-11 12:09:56 -0500

    rendered browable api is enable

--- server/server/settings.py
+++ server/server/settings.py
@@ -169,7 +169,7 @@ REST_FRAMEWORK = {
 
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
-        # 'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
     ),
 
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend',]
