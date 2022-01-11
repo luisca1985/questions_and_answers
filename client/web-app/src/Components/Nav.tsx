08315dd73f4719e4ae29e55a58ceda20d88aec5c
Luis Camilo Jimenez Alvarez <luisca1985@gmail.com>
2022-01-11 07:08:06 -0500

    reorder & text adjust nav bar

--- client/web-app/src/Components/Nav.tsx
+++ client/web-app/src/Components/Nav.tsx
@@ -4,14 +4,18 @@ import { Link } from 'react-router-dom';
 
 
 const Nav = () => {
-    const options = [{value:"Home", url:"/"},{value:"Show questions", url:"/questions"},{value:"Create question", url:"/questions/create"}]
+    const options = [
+        { value: "Home", url: "/" },
+        { value: "Show questions", url: "/questions" },
+        { value: "Create new question", url: "/questions/create" }
+    ]
     return (
         <div className="nav-scroller py-1 mb-2">
             <nav className="nav d-flex justify-content-between">
                 {
-                options.map((option, index)=>
-                    <Link key={index} to={option.url} className="p-2 link-secondary">{option.value}</Link>
-                )
+                    options.map((option, index) =>
+                        <Link key={index} to={option.url} className="p-2 link-secondary">{option.value}</Link>
+                    )
                 }
             </nav>
         </div>
