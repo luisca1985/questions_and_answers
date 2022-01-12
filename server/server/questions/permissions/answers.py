cd25a08ee95cbca8506cbfffd3025aa9e0c9e832
Luis Camilo Jimenez Alvarez <luisca195@gmail.com>
2022-01-12 03:27:17 -0500

    enable create answers with token

--- server/server/questions/permissions/answers.py
+++ server/server/questions/permissions/answers.py
@@ -5,3 +5,5 @@ class AnswerPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         if view.action in ['list','list_for_question']:
             return True
+        if view.action in ['create']:
+            return request.user.is_authenticated
