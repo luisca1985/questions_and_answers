acff75ebd366d90d77388480b5b91832b6cc5ad9
Luis Camilo Jimenez Alvarez <luisca195@gmail.com>
2022-01-12 03:25:02 -0500

    use token, and testing redirect options

--- client/web-app/src/Modules/QuestionsAnswersCreate.tsx
+++ client/web-app/src/Modules/QuestionsAnswersCreate.tsx
@@ -1,24 +1,41 @@
 import React, { SyntheticEvent, useState } from 'react';
-import {useParams} from 'react-router-dom';
+import { Navigate, useParams } from 'react-router-dom';
 
 const QuestionsAnswersCreate: React.FC = () => {
     const questionId = useParams().id
     const question_to_answer = questionId
     const [detail, setDetail] = useState('');
     const answered_by = 1;
+    const [redirect, setRedirect] = useState(false);
+
+    // Provisional token
+    const [token, setToken] = useState('')
+    // Provisional token end
 
     const submit = async (e: SyntheticEvent) => {
         e.preventDefault();
-        await fetch(`/api/answers/`,{
+        await fetch(`/api/answers/`, {
             method: 'POST',
-            headers: {'Content-Type': 'application/json'},
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Token ${token}`
+            },
             body: JSON.stringify({
                 detail,
                 question_to_answer,
                 answered_by
             })
+        }).then(response => {
+            if (response.status){
+                console.log(response.status);
+                setRedirect(true);
+            }
         })
     }
+    
+    // if (redirect) {
+    //     return <Navigate to={`/questions/${ questionId }/answers/`} />
+    // }
 
     return (
         <div>
@@ -31,6 +48,15 @@ const QuestionsAnswersCreate: React.FC = () => {
                 </div>
                 <button className="btn btn-outline-secondary">Create new answer</button>
             </form>
+            {/* Provisional token */}
+            <div className="input-group mb-3">
+                <div className="input-group-prepend">
+                    <span className="input-group-text" id="basic-addon3">Authorization Token</span>
+                </div>
+                <input type="text" className="form-control" id="basic-url"
+                    aria-describedby="basic-addon3" onChange={e => setToken(e.target.value)} />
+            </div>
+            {/* Provisional token end */}
         </div>
     );
 };
