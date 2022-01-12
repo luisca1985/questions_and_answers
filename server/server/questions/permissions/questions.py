bc4365de8716d181edb853c507c4b390888d67a5
Luis Camilo Jimenez Alvarez <luisca1985@gmail.com>
2022-01-11 22:59:01 -0500

    allow authentication to create question & answers

--- server/server/questions/permissions/questions.py
+++ server/server/questions/permissions/questions.py
@@ -4,4 +4,6 @@ class QuestionPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
         if view.action in ['list','retrieve']:
-            return True
+             return True
+        if view.action in ['create']:
+            return request.user.is_authenticated
